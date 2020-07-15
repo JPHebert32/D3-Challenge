2cdfb92ff2d0e40e3490288bd2b8b57b85f62570
JP Hebert <JPHebert32@gmail.com>
2020-07-15 11:54:09 -0600

    insert axis

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,149 @@
-// @TODO: YOUR CODE HERE!
+// The code for the chart is wrapped inside a function that
+// automatically resizes the chart
+function makeResponsive() {
+
+  // if the SVG area isn't empty when the browser loads,
+  // remove it and replace it with a resized version of the chart
+  var svgArea = d3.select("body").select("svg");
+
+  // clear svg is not empty
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
+
+  // SVG wrapper dimensions are determined by the current width and
+  // height of the browser window.
+  var svgWidth = window.innerWidth;
+  var svgHeight = window.innerHeight;
+
+  var margin = {
+    top: 50,
+    bottom: 50,
+    right: 50,
+    left: 50
+  };
+
+  var height = svgHeight - margin.top - margin.bottom;
+  var width = svgHeight - margin.left - margin.right;
+
+  // Append SVG element
+  var svg = d3
+    .select("#chart")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+  // Append group element
+  var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import data from an external CSV file
+d3.csv("assets/data/data.csv").then(function(healthData) {
+  console.log(healthData);
+  console.log([healthData]);
+
+  // parse data
+   healthData.forEach(function(data) {
+     data.healthcare = +data.age;
+     data.poverty = +data.smokes;
+   });
+
+   // create scales
+   var xTimeScale = d3.scaleTime()
+     .domain(d3.extent(healthData, d => d.age))
+     .range([0, width]);
+
+   var yLinearScale = d3.scaleLinear()
+     .domain([0, d3.max(healthData, d => d.smokes)])
+     .range([height, 0]);
+
+   // create axes
+   var xAxis = d3.axisBottom(xTimeScale);
+   var yAxis = d3.axisLeft(yLinearScale);
+
+   // append axes
+   chartGroup.append("g")
+     .attr("transform", `translate(0, ${height})`)
+     .style("font-size", "16px")
+     .call(xAxis);
+
+   chartGroup.append("g")
+     .style("font-size", "16px")
+     .call(yAxis);
+
+     // function for circles
+   chartGroup.selectAll("circle")
+     .data(healthData)
+     .enter()
+     .append("circle")
+     .attr("cx", d => xLinearScale(d.age))
+     .attr("cy", d => yLinearScale(d.smokes))
+     .attr("r", 12)
+     .attr("fill", "blue")
+     .attr("opacity", ".6");
+
+     // add State abbrev to circles
+ chartGroup.selectAll("text.text-circles")
+     .data(myData)
+     .enter()
+     .append("text")
+     .classed("text-circles",true)
+     .text(d => d.abbr)
+     .attr("x", d => xLinearScale(d.age))
+     .attr("y", d => yLinearScale(d.somkes))
+     .attr("dy",5)
+     .attr("text-anchor","middle")
+     .attr("font-size","12px")
+     .attr("fill", "white");
+
+ // y axis
+ chartGroup.append("text")
+     .attr("transform", "rotate(-90)")
+     .attr("y", 30 - margin.left)
+     .attr("x", 0 - (height / 2))
+     .attr("dy", "1em")
+     .classed("aText", true)
+     .text("Lacks Healthcare (%)");
+
+ // x axis
+ chartGroup.append("text")
+     .attr("y", height + margin.bottom/2 - 10)
+     .attr("x", width / 2)
+     .attr("dy", "1em")
+     .classed("aText", true)
+     .text("Poverty Rate (%)");
+
+   }).catch(function(error) {
+  console.log(error);
+});
+}
+
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);
+
+
+
+
+// columns: Array(19)
+// 0: "id"
+// 1: "state"
+// 2: "abbr"
+// 3: "poverty"
+// 4: "povertyMoe"
+// 5: "age"
+// 6: "ageMoe"
+// 7: "income"
+// 8: "incomeMoe"
+// 9: "healthcare"
+// 10: "healthcareLow"
+// 11: "healthcareHigh"
+// 12: "obesity"
+// 13: "obesityLow"
+// 14: "obesityHigh"
+// 15: "smokes"
+// 16: "smokesLow"
+// 17: "smokesHigh"
+// 18: "-0.385218228"
